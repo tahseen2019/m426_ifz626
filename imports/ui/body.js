c92685d662b870005c4b8e0876d4ed9bdb466331
hamza <hamza-mulahmetovic@hotmail.com>
2018-07-04 14:27:18 +0200

    changed body.js

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -2,38 +2,27 @@ import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { ReactiveDict } from 'meteor/reactive-dict';
 
-import { Tasks } from '../api/tasks.js';
+
 import { Messages } from '../api/messages.js';
 
-import './task.js';
+import './messages.js';
 import './chat.js';
 import './body.html';
 
 
 Template.body.onCreated(function bodyOnCreated() {
   this.state = new ReactiveDict();
-  Meteor.subscribe('tasks');
+  Meteor.subscribe('messages');
 });
 
-Template.body.helpers({
-  tasks() {
-    const instance = Template.instance();
-    if (instance.state.get('hideCompleted')) {
-      // If hide completed is checked, filter tasks
-      return Tasks.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } });
-    }
-    // Otherwise, return all of the tasks
-    return Tasks.find({}, { sort: { createdAt: -1 } });
-  },
-  incompleteCount() {
-    return Tasks.find({ checked: { $ne: true } }).count();
-  },
-});
 
 Template.body.events({
   'submit .new-task'(event) {
     // Prevent default browser form submit
     event.preventDefault();
+
+
+    console.log("submit .new-message");
 
     // Get value from form element
     const target = event.target;
@@ -53,8 +42,5 @@ Template.body.events({
 
     // Clear form
     target.text.value = '';
-  },
-  'change .hide-completed input'(event, instance) {
-    instance.state.set('hideCompleted', event.target.checked);
   },
 });
