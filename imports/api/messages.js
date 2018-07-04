8af89de1ed8a8e2b276609e2b75942fc902512af
hamza <hamza-mulahmetovic@hotmail.com>
2018-07-04 14:05:18 +0200

    finished messages.js

--- imports/api/messages.js
+++ imports/api/messages.js
@@ -4,8 +4,19 @@ import { check } from 'meteor/check';
 
 export const Messages = new Mongo.Collection('messages');
 
+var myData = {
+              idChat: this.idChat,
+              idSender: this.userID,
+              msg: "test",
+              sentAt: new Date(),
+            }
+
+
+
+Messages.insert(myData);
+
 Meteor.methods({
-  'messages.insert'(text) {
+  'messages.insert'(chatId, senderId, text, sentAt) {
     check(text, String);
 
     // Make sure the user is logged in before inserting a task
@@ -14,10 +25,10 @@ Meteor.methods({
     }
 
     Messages.insert({
-      idChat: this.idChat,
-      idSender: this.userId,
+      idChat: chatId,
+      idSender: senderId,
       msg: text,
-      sentAt: new Date(),
+      sentAt: sentAt,
     });
   },
   'messages.find'(chatId) {
